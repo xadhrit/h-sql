1a6ebd3050e543726c6b0da3c97a7f55054353ae
xadhrit <maharajrabel@gmail.com>
2021-08-16 14:38:56 -0400

    scripts

---
+++ CMakeFiles/h-sql.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h-sql.dir/db.c.o"
+  "CMakeFiles/h-sql.dir/hashdb.c.o"
+  "h-sql"
+  "h-sql.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/h-sql.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/h-sql.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for h-sql.
+# This may be replaced when dependencies are built.
